b11bc104cd27a048e3d04ebd696e88844f7e9662
Alex Freska <alex.freska@gmail.com>
2022-01-14 14:03:01 -0500

    Fix FOUC issue, Grid, remove API routes (#17)

--- src/pages/api/siastats/hosts/active.ts
+++
@@ -1,6 +0,0 @@
-import { getSiaStatsHostsActive } from '../../../../lib/data/siaStats/hostsActive'
-
-export default async function handler(req, res) {
-  const { status, data } = await getSiaStatsHostsActive()
-  res.status(status).json(data)
-}
--- src/pages/api/siastats/hosts/coordinates.ts
+++
@@ -1,6 +0,0 @@
-import { getSiaStatsHostsCoordinates } from '../../../../lib/data/siaStats/hostsCoordinates'
-
-export default async function handler(req, res) {
-  const { status, data } = await getSiaStatsHostsCoordinates()
-  res.status(status).json(data)
-}
