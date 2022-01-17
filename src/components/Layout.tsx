2fe39274258b77d416d93af7a744e5a2a7480593
Alex Freska <alex.freska@gmail.com>
2022-01-17 13:03:13 -0500

    Set up matomo, fix static server too many redirects issue (#19)

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,4 +1,5 @@
 import Head from 'next/head'
+import Script from 'next/script'
 import { appName } from '../config/app'
 import { Box } from '../system/Box'
 import { Container } from '../system/Container'
@@ -44,7 +45,33 @@ export function Layout({ children }: Props) {
         <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#1ed660" />
         <meta name="msapplication-TileColor" content="#2b5797" />
         <meta name="theme-color" content="#ffffff" />
+        <noscript>
+          <img
+            referrerPolicy="no-referrer-when-downgrade"
+            src="https://surveillance.sia.tech/matomo.php?idsite=1&amp;rec=1"
+            style={{ border: 0 }}
+            alt=""
+          />
+        </noscript>
       </Head>
+      <Script
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+              var _paq = window._paq = window._paq || [];
+              /* tracker methods like "setCustomDimension" should be called before "trackPageView" */
+              _paq.push(['trackPageView']);
+              _paq.push(['enableLinkTracking']);
+              (function() {
+                var u="//surveillance.sia.tech/";
+                _paq.push(['setTrackerUrl', u+'matomo.php']);
+                _paq.push(['setSiteId', '1']);
+                var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+                g.async=true; g.src=u+'matomo.js'; s.parentNode.insertBefore(g,s);
+              })();
+              `,
+        }}
+      />
       <Container size="3">
         <Flex as="main" direction="column" gap="8" css={{ margin: '$5 0' }}>
           <Header />
