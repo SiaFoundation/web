66da5bb02aca474537d3dba17f458d42fec2981b
Alex Freska <alex.freska@gmail.com>
2022-02-15 11:08:03 -0500

    chore!: rename sia-js to sia-nodejs

---
+++ libs/sia-nodejs/README.md
@@ -0,0 +1,7 @@
+# sia-nodejs
+
+Core Sia types and library methods for interacting with `siad`.
+
+## Running unit tests
+
+Run `nx test sia-nodejs` to execute the unit tests via [Jest](https://jestjs.io).
--- libs/sia-nodejs/jest.config.js
+++ libs/sia-nodejs/jest.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  displayName: 'sia-js',
+  displayName: 'sia-nodejs',
   preset: '../../jest.preset.js',
   globals: {
     'ts-jest': {
@@ -11,5 +11,5 @@ module.exports = {
     '^.+\\.[tj]sx?$': 'ts-jest',
   },
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
-  coverageDirectory: '../../coverage/libs/sia-js',
+  coverageDirectory: '../../coverage/libs/sia-nodejs',
 }
