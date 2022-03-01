0011b9f9b41518f2362a2fa6e620dbcde8e7178d
Alex Freska <alex.freska@gmail.com>
2022-03-01 14:04:50 -0500

    feat: add file dropzone component, add separator to user menu theme group

---
+++ libs/design-system/src/components/Dropzone.tsx
@@ -0,0 +1,47 @@
+import { useCallback } from 'react'
+import { useDropzone } from 'react-dropzone'
+import { Box } from '../primitives/Box'
+import { Paragraph } from '../primitives/Paragraph'
+
+type Props = {
+  title?: string
+  onFiles: (files: File[]) => void
+}
+
+export function Dropzone({ title, onFiles }: Props) {
+  const onDrop = useCallback(
+    (acceptedFiles: File[]) => {
+      onFiles(acceptedFiles)
+    },
+    [onFiles]
+  )
+
+  const { getRootProps, getInputProps, isDragActive } = useDropzone({ onDrop })
+
+  return (
+    <Box
+      {...getRootProps()}
+      css={{
+        border: '3px solid $primary4',
+        width: '100%',
+        height: '100px',
+        borderRadius: '$3',
+        borderStyle: 'dashed',
+        padding: '$3',
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        textAlign: 'center',
+        ...(isDragActive && {
+          border: '3px solid $primary10',
+          backgroundColor: '$primary1',
+        }),
+      }}
+    >
+      <input {...getInputProps()} />
+      <Paragraph size="1">
+        {title || 'Drop your files here or click to to open the file picker.'}
+      </Paragraph>
+    </Box>
+  )
+}
