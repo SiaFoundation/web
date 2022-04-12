2dcd82f059fc0caec6a78af412d374481081272e
Alex Freska <alex.freska@gmail.com>
2022-04-12 11:57:04 -0400

    feat: deploy script

---
+++ server/deploy.sh
@@ -0,0 +1,11 @@
+#!/bin/sh -eu
+# deploy web
+
+cd ~/projects/web
+git fetch origin
+if git status | grep -q behind; then
+  git merge origin/main
+  npm i
+	nx build --affected
+  pm2 reload server/pm2.config.js
+fi
