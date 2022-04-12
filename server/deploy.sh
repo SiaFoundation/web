80f9bcc3ca6fa44928c3424a02c57ff631f261db
Alex Freska <alex.freska@gmail.com>
2022-04-12 16:36:17 -0400

    fix: deploy script

--- server/deploy.sh
+++ server/deploy.sh
@@ -1,11 +1,10 @@
-#!/bin/sh -eu
-# deploy web
+#!/bin/bash -eu
 
 cd ~/projects/web
 git fetch origin
 if git status | grep -q behind; then
   git merge origin/main
   npm i
-	nx build --affected
+  nx build --affected
   pm2 reload server/pm2.config.js
 fi
