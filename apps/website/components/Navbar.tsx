a439a7be9e382896f7b322715ffc09c97cb6d585
Alex Freska <alex.freska@gmail.com>
2022-04-26 17:40:39 -0400

    fix: remove nav logo letter navigation

--- apps/website/components/Navbar.tsx
+++ apps/website/components/Navbar.tsx
@@ -7,31 +7,8 @@ import {
 } from '@siafoundation/design-system'
 import wordmark from '../assets/wordmark.svg'
 
-type Props = {
-  onClick?: () => void
-}
-
-export function Navbar({ onClick }: Props) {
+export function Navbar() {
   const { activeTheme } = useTheme()
-
-  if (onClick) {
-    return (
-      <Flex align="center" justify="between">
-        <Heading size="32" onClick={onClick} css={{ cursor: 'pointer' }}>
-          <Box
-            as="img"
-            src={wordmark.src}
-            css={{
-              filter: `invert(${activeTheme === 'dark' ? 1 : 0})`,
-              height: '39px',
-              width: '65px',
-            }}
-            alt="Sia"
-          />
-        </Heading>
-      </Flex>
-    )
-  }
 
   return (
     <Flex align="center" justify="between">
