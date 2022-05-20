764fef16a3e38ec2ed07bfc9261b97941c693f6e
Alex Freska <alex.freska@gmail.com>
2022-05-20 10:35:34 -0400

    fix: remove unused menu components

--- libs/design-system/src/core/Menu.tsx
+++ libs/design-system/src/core/Menu.tsx
@@ -1,10 +1,4 @@
-import React from 'react'
-import * as MenuPrimitive from '@radix-ui/react-menu'
-import { CheckIcon } from '@radix-ui/react-icons'
-import { styled, css, CSS } from '../config/theme'
-import { Box } from './Box'
-import { Flex } from './Flex'
-import { panelStyles } from './Panel'
+import { css } from '../config/theme'
 
 export const baseItemCss = css({
   display: 'flex',
@@ -55,72 +49,3 @@ export const separatorCss = css({
   my: '$1',
   backgroundColor: '$separator',
 })
-
-export const Menu = styled(MenuPrimitive.Root, menuCss)
-export const MenuContent = styled(MenuPrimitive.Content, panelStyles)
-
-export const MenuSeparator = styled(MenuPrimitive.Separator, separatorCss)
-
-export const MenuItem = styled(MenuPrimitive.Item, itemCss)
-
-const StyledMenuRadioItem = styled(MenuPrimitive.RadioItem, itemCss)
-
-type MenuRadioItemPrimitiveProps = React.ComponentProps<
-  typeof MenuPrimitive.RadioItem
->
-type MenuRadioItemProps = MenuRadioItemPrimitiveProps & { css?: CSS }
-
-export const MenuRadioItem = React.forwardRef<
-  React.ElementRef<typeof StyledMenuRadioItem>,
-  MenuRadioItemProps
->(({ children, ...props }, forwardedRef) => (
-  <StyledMenuRadioItem {...props} ref={forwardedRef}>
-    <Box css={{ position: 'absolute', left: '$1' }}>
-      <MenuPrimitive.ItemIndicator>
-        <Flex
-          css={{
-            width: '$2',
-            height: '$2',
-            alignItems: 'center',
-            justifyContent: 'center',
-          }}
-        >
-          <Box
-            css={{
-              width: '$1',
-              height: '$1',
-              backgroundColor: 'currentColor',
-              borderRadius: '$round',
-            }}
-          ></Box>
-        </Flex>
-      </MenuPrimitive.ItemIndicator>
-    </Box>
-    {children}
-  </StyledMenuRadioItem>
-))
-
-const StyledMenuCheckboxItem = styled(MenuPrimitive.CheckboxItem, itemCss)
-
-type MenuCheckboxItemPrimitiveProps = React.ComponentProps<
-  typeof MenuPrimitive.CheckboxItem
->
-type MenuCheckboxItemProps = MenuCheckboxItemPrimitiveProps & { css?: CSS }
-
-export const MenuCheckboxItem = React.forwardRef<
-  React.ElementRef<typeof StyledMenuCheckboxItem>,
-  MenuCheckboxItemProps
->(({ children, ...props }, forwardedRef) => (
-  <StyledMenuCheckboxItem {...props} ref={forwardedRef}>
-    <Box css={{ position: 'absolute', left: '$1' }}>
-      <MenuPrimitive.ItemIndicator>
-        <CheckIcon />
-      </MenuPrimitive.ItemIndicator>
-    </Box>
-    {children}
-  </StyledMenuCheckboxItem>
-))
-
-export const MenuLabel = styled(MenuPrimitive.Label, labelCss)
-export const MenuRadioGroup = styled(MenuPrimitive.RadioGroup, {})
-export const MenuGroup = styled(MenuPrimitive.Group, {})
