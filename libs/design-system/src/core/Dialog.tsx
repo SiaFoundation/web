30791602d7c7c220be7e9bd90c440d084866197c
Alex Freska <alex.freska@gmail.com>
2022-05-23 17:32:01 -0400

    fix: remove unused code

--- libs/design-system/src/core/Dialog.tsx
+++ libs/design-system/src/core/Dialog.tsx
@@ -98,8 +98,6 @@ export const dialogContentStyles = css({
   transition: 'border 0.3s ease-in-out, box-shadow 0.3s ease-in-out',
   color: '$hiContrast',
   borderRadius: '$2',
-  // boxShadow:
-  //   'hsl(206 22% 7% / 35%) 0px 10px 38px -10px, hsl(206 22% 7% / 20%) 0px 10px 20px -15px',
   boxShadow: '$colors$shadowActive',
   zIndex: 3,
   position: 'fixed',
