6069296e63d69c70ccc74ffd13a8e30087ad7535
Alex Freska <alex.freska@gmail.com>
2022-05-23 16:21:04 -0400

    feat: update dialog components

--- libs/design-system/src/core/AlertDialog.tsx
+++ libs/design-system/src/core/AlertDialog.tsx
@@ -7,38 +7,82 @@ import {
   dialogOverlayStyles,
   dialogTitleCss,
 } from './Dialog'
+import { Box } from './Box'
+import { Flex } from './Flex'
 
-type AlertDialogProps = React.ComponentProps<
-  typeof AlertDialogPrimitive.Root
-> & {
-  children: React.ReactNode
-}
-
-export const AlertDialogOverlay = styled(
+const AlertDialogOverlay = styled(
   AlertDialogPrimitive.Overlay,
   dialogOverlayStyles
 )
 
-export function AlertDialog({ children, ...props }: AlertDialogProps) {
-  return (
-    <AlertDialogPrimitive.Root {...props}>{children}</AlertDialogPrimitive.Root>
-  )
-}
-
-export const AlertDialogContent = styled(
+const StyledAlertDialogContent = styled(
   AlertDialogPrimitive.Content,
   dialogContentStyles
 )
 
-export const AlertDialogTrigger = AlertDialogPrimitive.Trigger
-export const AlertDialogPortal = AlertDialogPrimitive.Portal
-export const AlertDialogTitle = styled(
-  AlertDialogPrimitive.Title,
-  dialogTitleCss
-)
-export const AlertDialogDescription = styled(
+const AlertDialogPortal = AlertDialogPrimitive.Portal
+const AlertDialogTitle = styled(AlertDialogPrimitive.Title, dialogTitleCss)
+const AlertDialogDescription = styled(
   AlertDialogPrimitive.Description,
   dialogDescriptionCss
 )
-export const AlertDialogAction = AlertDialogPrimitive.Action
-export const AlertDialogCancel = AlertDialogPrimitive.Cancel
+const AlertDialogAction = AlertDialogPrimitive.Action
+const AlertDialogCancel = AlertDialogPrimitive.Cancel
+
+type DialogControlsProps = {
+  children: React.ReactNode
+  separator?: boolean
+}
+
+function DialogControls({ children, separator = true }: DialogControlsProps) {
+  return (
+    <Box
+      css={{
+        padding: '$1 0',
+        margin: '0 $2',
+        backgroundColor: '$loContrast',
+        borderTop: separator ? '1px solid $brandGray3' : 'none',
+      }}
+    >
+      {children}
+    </Box>
+  )
+}
+
+type AlertDialogContentProps = AlertDialogPrimitive.AlertDialogContentProps & {
+  title: React.ReactNode
+  description: React.ReactNode
+  action: React.ReactNode
+  cancel: React.ReactNode
+}
+
+export function AlertDialogContent({
+  children,
+  title,
+  description,
+  action,
+  cancel,
+  ...props
+}: AlertDialogContentProps) {
+  return (
+    <AlertDialogPortal>
+      <AlertDialogOverlay />
+      <StyledAlertDialogContent {...props}>
+        <AlertDialogTitle>{title}</AlertDialogTitle>
+        <Box css={{ padding: '0 $2 $2' }}>
+          <AlertDialogDescription>{description}</AlertDialogDescription>
+          {children}
+        </Box>
+        <DialogControls separator>
+          <Flex gap="1" justify="end">
+            <AlertDialogCancel asChild>{cancel}</AlertDialogCancel>
+            <AlertDialogAction asChild>{action}</AlertDialogAction>
+          </Flex>
+        </DialogControls>
+      </StyledAlertDialogContent>
+    </AlertDialogPortal>
+  )
+}
+
+export const AlertDialog = AlertDialogPrimitive.Root
+export const AlertDialogTrigger = AlertDialogPrimitive.Trigger
