d67b0e245a9198843dd748022321716f4a4b9ecc
Alex Freska <alex.freska@gmail.com>
2022-05-24 14:52:02 -0400

    fix: remove comments

--- apps/website/content/stats.ts
+++ apps/website/content/stats.ts
@@ -10,23 +10,23 @@ import { humanBytes, humanSpeed } from '@siafoundation/sia-js'
 import { AsyncReturnType } from '../lib/types'
 
 export async function getStats() {
-  // if (isDev()) {
-  //   return {
-  //     activeHosts: '20,531',
-  //     onlineHosts: '20,634',
-  //     totalStorage: '207.38 PB',
-  //     usedStorage: '202.44 PB',
-  //     commits: '2,020,909',
-  //     contributors: '2,069',
-  //     forks: '20,472',
-  //     releases: '2,041',
-  //     downloads: '201,059,994',
-  //     downloadSpeed: '201.44 Gbps',
-  //     uploadSpeed: '2071.08 Mbps',
-  //     cpuUsage: '20.147%',
-  //     memoryUsage: '201.69 MB',
-  //   }
-  // }
+  if (isDev()) {
+    return {
+      activeHosts: '20,531',
+      onlineHosts: '20,634',
+      totalStorage: '207.38 PB',
+      usedStorage: '202.44 PB',
+      commits: '2,020,909',
+      contributors: '2,069',
+      forks: '20,472',
+      releases: '2,041',
+      downloads: '201,059,994',
+      downloadSpeed: '201.44 Gbps',
+      uploadSpeed: '2071.08 Mbps',
+      cpuUsage: '20.147%',
+      memoryUsage: '201.69 MB',
+    }
+  }
 
   const [hostsStats, explore, downloadCounts, github, benchmarks] =
     await Promise.all([
@@ -44,14 +44,10 @@ export async function getStats() {
     activeHosts: formatNumber(hostsStats.data?.totals.active_hosts),
     onlineHosts: formatNumber(hostsStats.data?.totals.total_hosts),
     // storage
-    totalStorage: humanBytes(
-      hostsStats.data?.totals.total_storage
-      // hostsStats.data?.totalstorage * 1000 * 1000 * 1000 * 1000
-    ),
+    totalStorage: humanBytes(hostsStats.data?.totals.total_storage),
     usedStorage: humanBytes(
       hostsStats.data?.totals.total_storage -
         hostsStats.data?.totals.remaining_storage
-      // hostsStats.data?.usedstorage * 1000 * 1000 * 1000 * 1000
     ),
     totalRegistry: formatNumber(
       (hostsStats.data?.totals.total_registry_entries || 0) / 1_000_000,
