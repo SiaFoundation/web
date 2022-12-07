bb2047d534aa08a9877bf04c5b1c79414b9d51e2
Alex Freska <alex.freska@gmail.com>
2022-12-07 14:36:22 -0500

    fix: next/image 13 API changes

--- libs/design-system/src/core/Image.tsx
+++ libs/design-system/src/core/Image.tsx
@@ -1,11 +1,12 @@
 import NextImage from 'next/image'
-import { cva, VariantProps } from 'class-variance-authority'
+import { cva } from 'class-variance-authority'
 import React from 'react'
+import { VariantProps } from '../types'
 
 const styles = cva(['rounded'])
 
-type Props = React.ComponentProps<typeof NextImage> &
-  VariantProps<typeof styles>
+type Props = VariantProps<typeof styles> &
+  React.ComponentProps<typeof NextImage>
 
 export function Image({ className, ...props }: Props) {
   return <NextImage className={styles({ className })} {...props} />
