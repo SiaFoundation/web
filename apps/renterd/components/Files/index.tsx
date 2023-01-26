47acbd63b5585cdffc4798cafb987adadadadb48
Alex Freska <alex.freska@gmail.com>
2023-01-26 15:56:45 -0500

    feat: file explorer and upload manager

---
+++ apps/renterd/components/Files/index.tsx
@@ -0,0 +1,27 @@
+import { RenterSidenav } from '../RenterSidenav'
+import { routes } from '../../config/routes'
+import { useDialog } from '../../contexts/dialog'
+import { FilesExplorer } from './FilesExplorer'
+import { FileNav } from '../FileNav'
+import { RenterdAuthedLayout } from '../RenterdAuthedLayout'
+import { FilesActions } from './FilesActions'
+
+export function Files() {
+  const { openDialog } = useDialog()
+
+  return (
+    <RenterdAuthedLayout
+      routes={routes}
+      sidenav={<RenterSidenav />}
+      filters={
+        <>
+          <FileNav />
+        </>
+      }
+      actions={<FilesActions />}
+      openSettings={() => openDialog('settings')}
+    >
+      <FilesExplorer />
+    </RenterdAuthedLayout>
+  )
+}
