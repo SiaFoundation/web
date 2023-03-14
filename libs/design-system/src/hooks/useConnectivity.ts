64caef6dd3c1d9d85d4eb8802baa799ed0efbce2
Alex Freska <alex.freska@gmail.com>
2023-03-14 08:42:39 -0400

    fix: connectivity polling

--- libs/design-system/src/hooks/useConnectivity.ts
+++ libs/design-system/src/hooks/useConnectivity.ts
@@ -1,7 +1,13 @@
 import { useConsensusState } from '@siafoundation/react-core'
 
 export function useConnectivity() {
-  const w = useConsensusState()
+  const w = useConsensusState({
+    config: {
+      swr: {
+        refreshInterval: (data) => (data?.Synced ? 30_000 : 3_000),
+      },
+    },
+  })
   return {
     isConnected: !w.error,
     isSynced: w.data?.Synced,
