ad84f53c22587efad9657978640144e5202d16c8
Alex Freska <alex.freska@gmail.com>
2023-04-24 09:39:46 -0400

    chore: export renterd/v0.12.0

---
+++ script.sh
@@ -0,0 +1,54 @@
+APPS="renterd,hostd,walletd"
+go_releases=()
+for app in $(echo $APPS | tr ',' ' ')
+do
+  echo "$app"
+  latest_release=$(git describe --tags --match "$app@*" --abbrev=0 2>/dev/null)
+  if [ -z "$latest_release" ]
+  then
+    echo "Latest release for $app does not exist"
+  else
+    echo "Latest release: $latest_release"
+    version=$(echo $latest_release | cut -d "@" -f 2)
+    go_release="$app/v$version"
+    tag_exists=$(git describe --exact-match "$go_release" 2>/dev/null)
+    if [ -z "$tag_exists" ]
+    then
+      echo "Tag $go_release exists"
+    else
+      echo "Tag $go_release does not exists, exporting app"
+      npx nx export $app
+      mkdir -p $app/ui/assets
+      rm -rf $app/ui/assets/*
+      cp -R dist/apps/$app-embed/exported/* $app/ui/assets/
+      go_releases+=("$go_release")
+    fi
+  fi
+done
+
+if [ ${#go_releases[@]} -gt 0 ]
+then
+  echo "Released: ${go_releases[*]}"
+  git add .
+  git commit -m "chore: export ${go_releases[*]}"
+  for tag in "${go_releases[@]}"
+  do
+    git tag -a $tag -m "Go module $tag"
+  done
+fi
+
+# # check out the commit as a new branch
+# git checkout -b release
+# # sync up the main branch in case something has been merged
+# # since the action started
+# git fetch origin main
+# # checkout the main branch and merge in the release branch
+# # this will be a fast forward merge unless something went wrong
+# git checkout main
+# git merge release
+# # push the main branch
+# git push origin main
+# git push origin ${TAG}
+# # cleanup
+# git branch -D release
+#
