ad56d4aef2d8285288c2a30b0336bc8fa98bf053
alexfreska <alexfreska@users.noreply.github.com>
2023-04-24 18:23:05 +0000

    chore: export hostd/v0.7.0

---
+++ hostd/ui/assets/_next/static/chunks/pages/config-540d1cdc2445e873.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[490],{57344:function(n,u,t){(window.__NEXT_P=window.__NEXT_P||[]).push(["/config",function(){return t(96745)}])},96745:function(n,u,t){"use strict";function _(){return"hello"}t.r(u),t.d(u,{default:function(){return _}})}},function(n){n.O(0,[774,888,179],function(){return n(n.s=57344)}),_N_E=n.O()}]);
