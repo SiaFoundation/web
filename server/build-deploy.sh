477e0c5223dd004b0711b04df2df7bc419795397
Alex Freska <alex.freska@gmail.com>
2023-09-27 17:17:56 -0400

    feat: grants

--- server/build-deploy.sh
+++ server/build-deploy.sh
@@ -1,5 +1,4 @@
 #!/bin/bash -eu
 
-nx run-many --target=build --projects=website,assets,crons,renterd,hostd,walletd,explorer --prod
-nx run explorer:build:production-testnet
+nx run-many --target=build --projects=website,assets,crons,renterd,hostd,walletd --prod
 pm2 reload server/pm2.config.js
