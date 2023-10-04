62aed749581e64743baf989e7d05d2ec7d75d6e6
Alex Freska <alex.freska@gmail.com>
2023-10-04 13:48:03 +0000

    ci: docker config

--- server/docker-deploy.sh
+++ server/docker-deploy.sh
@@ -1,3 +1,3 @@
 #!/bin/bash -eu
 
-docker compose up --force-recreate --pull web-website web-assets web-crons web-renterd web-hostd web-walletd web-explorer web-explorer-testnet-zen
+docker compose up --pull web-website web-assets web-crons web-renterd web-hostd web-walletd web-explorer web-explorer-testnet-zen
