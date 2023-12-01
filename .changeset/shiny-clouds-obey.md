ec8222d49c68f8df6c2a52c5428b3534f05ad673
Alex Freska <alex.freska@gmail.com>
2023-12-01 10:30:10 -0500

    fix: renterd uploads require sync

---
+++ .changeset/shiny-clouds-obey.md
@@ -0,0 +1,5 @@
+---
+'renterd': minor
+---
+
+File uploads are now disabled if the blockchain is not fully synced.
--- apps/renterd/components/Files/useCanUpload.tsx
+++ apps/renterd/components/Files/useCanUpload.tsx
@@ -1,14 +1,17 @@
+import { useSyncStatus } from '../../hooks/useSyncStatus'
 import { useFiles } from '../../contexts/files'
 import { useAutopilotNotConfigured } from './checks/useAutopilotNotConfigured'
 import { useNotEnoughContracts } from './checks/useNotEnoughContracts'
 
 export function useCanUpload() {
   const { isViewingABucket } = useFiles()
+  const syncStatus = useSyncStatus()
   const autopilotNotConfigured = useAutopilotNotConfigured()
   const notEnoughContracts = useNotEnoughContracts()
   return (
     isViewingABucket &&
     !autopilotNotConfigured.active &&
-    !notEnoughContracts.active
+    !notEnoughContracts.active &&
+    syncStatus.isSynced
   )
 }
