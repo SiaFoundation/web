0aaec35100e2a8be50fc97558c58baeced2f1293
Alex Freska <alex.freska@gmail.com>
2024-01-15 10:47:17 -0800

    fix: hostd navigation bug

--- apps/hostd/contexts/metrics/useNowAtInterval.tsx
+++ apps/hostd/contexts/metrics/useNowAtInterval.tsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from 'react'
 import { DataInterval, getDataIntervalInMs } from './types'
+import { hoursInMilliseconds } from '@siafoundation/design-system'
 
 // now timestamp updated every interval
 // used to reset the time range and keep the graph up to date
@@ -7,9 +8,15 @@ export function useNowAtInterval(dataInterval: DataInterval) {
   const [now, setNow] = useState(new Date().getTime())
   useEffect(() => {
     setNow(new Date().getTime())
+    // set a minimum refresh rate of every 1 hour
+    const minIntervalMs = hoursInMilliseconds(1)
+    const intervalMs = Math.min(
+      getDataIntervalInMs(dataInterval),
+      minIntervalMs
+    )
     const i = setInterval(() => {
       setNow(new Date().getTime())
-    }, getDataIntervalInMs(dataInterval))
+    }, intervalMs)
     return () => clearInterval(i)
   }, [dataInterval])
 
