5efb08acbb61985ca2276614b194c4ee445d1835
Alex Freska <alex.freska@gmail.com>
2024-02-22 10:01:42 -0500

    chore: class private method support to jest

--- apps/walletd/jest.config.ts
+++ apps/walletd/jest.config.ts
@@ -4,7 +4,13 @@ export default {
   preset: '../../jest.preset.js',
   transform: {
     '^(?!.*\\.(js|jsx|ts|tsx|css|json)$)': '@nx/react/plugins/jest',
-    '^.+\\.[tj]sx?$': ['babel-jest', { presets: ['@nx/next/babel'] }],
+    '^.+\\.[tj]sx?$': [
+      'babel-jest',
+      {
+        presets: ['@nx/next/babel'],
+        plugins: ['@babel/plugin-transform-private-methods'],
+      },
+    ],
   },
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
   coverageDirectory: '../../coverage/apps/walletd',
--- libs/sia-central/jest.config.ts
+++ libs/sia-central/jest.config.ts
@@ -3,7 +3,14 @@ export default {
   displayName: 'sia-central',
   preset: '../../jest.preset.js',
   transform: {
-    '^.+\\.[tj]sx?$': ['babel-jest', { presets: ['@nx/react/babel'] }],
+    '^(?!.*\\.(js|jsx|ts|tsx|css|json)$)': '@nx/react/plugins/jest',
+    '^.+\\.[tj]sx?$': [
+      'babel-jest',
+      {
+        presets: ['@nx/next/babel'],
+        plugins: ['@babel/plugin-transform-private-methods'],
+      },
+    ],
   },
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
   coverageDirectory: '../../coverage/libs/sia-central',
