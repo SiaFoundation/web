cae834e4d7f59249eb7396bec6edc0730919942e
Alex Freska <alex.freska@gmail.com>
2024-02-23 13:57:47 -0500

    feat: renterd warn active uploads

---
+++ .changeset/friendly-nails-marry.md
@@ -0,0 +1,5 @@
+---
+'renterd': minor
+---
+
+The browser now warns the user if they have active uploads and try to close the tab.
---
+++ apps/renterd/contexts/filesManager/useWarnActiveUploadsOnClose.tsx
@@ -0,0 +1,30 @@
+import { useEffect } from 'react'
+import { UploadsMap } from './types'
+
+export function useWarnActiveUploadsOnClose({
+  uploadsMap,
+}: {
+  uploadsMap: UploadsMap
+}) {
+  useEffect(() => {
+    const activeUploads = Object.values(uploadsMap).filter(
+      (upload) => upload.uploadStatus === 'uploading'
+    )
+
+    const warnUserAboutActiveUploads = (event: BeforeUnloadEvent) => {
+      if (activeUploads.length > 0) {
+        const message = `Warning, closing the tab will abort all ${activeUploads.length} active uploads.`
+        event.returnValue = message // Legacy method for cross browser support
+        return message // Chrome requires returnValue to be set
+      }
+    }
+
+    if (activeUploads.length > 0) {
+      window.addEventListener('beforeunload', warnUserAboutActiveUploads)
+    }
+
+    return () => {
+      window.removeEventListener('beforeunload', warnUserAboutActiveUploads)
+    }
+  }, [uploadsMap])
+}
