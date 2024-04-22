c031356cad9ef51468dabd515c33175f15f6e9df
Nate Maninger <me@n8m.us>
2024-04-22 09:04:26 -0700

    fix: add asset tests

---
+++ renterd/renterd_test.go
@@ -0,0 +1,30 @@
+package renterd
+
+import (
+	"io/fs"
+	"testing"
+
+	"go.sia.tech/web/internal/nextjs"
+)
+
+func TestRouter(t *testing.T) {
+	err := fs.WalkDir(assets, ".", func(path string, d fs.DirEntry, err error) error {
+		if err != nil {
+			t.Fatal(err)
+		}
+		t.Log(path)
+		return nil
+	})
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	assetFS, err := fs.Sub(assets, "assets")
+	if err != nil {
+		t.Fatal(err)
+	}
+	_, err = nextjs.NewRouter(assetFS.(fs.ReadDirFS))
+	if err != nil {
+		t.Fatal(err)
+	}
+}
