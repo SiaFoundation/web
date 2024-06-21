4a27247e5f26181e334dec0f009adb2495fc316c
Alex Freska <alex.freska@gmail.com>
2024-06-21 13:39:34 -0400

    fix: wallet event data types

--- apps/walletd/contexts/events/utils.ts
+++ apps/walletd/contexts/events/utils.ts
@@ -1,7 +1,27 @@
 import BigNumber from 'bignumber.js'
 import { WalletEvent } from '@siafoundation/walletd-types'
 
-export function calculateScOutflow(e: WalletEvent) {
+export function calculateScValue(e: WalletEvent) {
+  if (e.type === 'v2Transaction') {
+    return txnCalculateScInflow(e).minus(v2TxnCalculateScOutflow(e))
+  }
+  if (e.type === 'v1Transaction') {
+    return txnCalculateScInflow(e).minus(v1TxnCalculateScOutflow(e))
+  }
+  return new BigNumber(e.data.siacoinElement.siacoinOutput.value)
+}
+
+export function calculateSfValue(e: WalletEvent) {
+  if (e.type === 'v2Transaction') {
+    return txnCalculateSfInflow(e) - v2TxnCalculateSfOutflow(e)
+  }
+  if (e.type === 'v1Transaction') {
+    return txnCalculateSfInflow(e) - v1TxnCalculateSfOutflow(e)
+  }
+  return undefined
+}
+
+export function v1TxnCalculateScOutflow(e: WalletEvent) {
   if ('spentSiacoinElements' in e.data) {
     const siacoinElements = e.data.spentSiacoinElements
     return siacoinElements.reduce((acc, o) => {
@@ -13,7 +33,7 @@ export function calculateScOutflow(e: WalletEvent) {
   }
 }
 
-export function calculateSfOutflow(e: WalletEvent) {
+export function v1TxnCalculateSfOutflow(e: WalletEvent) {
   if ('spentSiafundElements' in e.data) {
     const siafundElements = e.data.spentSiafundElements || []
     return siafundElements.reduce((acc, o) => {
@@ -26,7 +46,21 @@ export function calculateSfOutflow(e: WalletEvent) {
   return 0
 }
 
-export function calculateScInflow(e: WalletEvent) {
+export function v2TxnCalculateScOutflow(e: WalletEvent) {
+  if (e.type === 'v2Transaction') {
+    return new BigNumber(e.data.siacoinInputs[0].parent.siacoinOutput.value)
+  }
+  return undefined
+}
+
+export function v2TxnCalculateSfOutflow(e: WalletEvent) {
+  if (e.type === 'v2Transaction') {
+    return e.data.siafundInputs[0].parent.siafundOutput.value
+  }
+  return undefined
+}
+
+export function txnCalculateScInflow(e: WalletEvent) {
   if ('transaction' in e.data) {
     const siacoinOutputs = e.data.transaction.siacoinOutputs || []
     return siacoinOutputs.reduce((acc, o) => {
@@ -39,7 +73,7 @@ export function calculateScInflow(e: WalletEvent) {
   return new BigNumber(0)
 }
 
-export function calculateSfInflow(e: WalletEvent) {
+export function txnCalculateSfInflow(e: WalletEvent) {
   if ('transaction' in e.data) {
     const siafundOutputs = e.data.transaction.siafundOutputs || []
     return siafundOutputs.reduce((acc, o) => {
@@ -53,17 +87,20 @@ export function calculateSfInflow(e: WalletEvent) {
 }
 
 export function getFee(e: WalletEvent) {
-  return 'transaction' in e.data
+  if (e.type === 'v2Transaction') {
+    return new BigNumber(e.data.minerFee)
+  }
+  return 'transaction' in e.data && e.data.transaction.minerFees?.length
     ? new BigNumber(e.data.transaction.minerFees[0])
     : undefined
 }
 
 export function getContractId(e: WalletEvent) {
   if (e.type === 'v1ContractResolution') {
-    return e.data.fileContract.id
+    return e.data.parent.id
   }
   if (e.type === 'v2ContractResolution') {
-    return e.data.fileContract.id
+    return e.data.parent.id
   }
   return undefined
 }
