da676e632f8223d59fe3847711a8751c5a29a82d
Alex Freska <alex.freska@gmail.com>
2024-07-12 09:54:31 -0400

    ci: remove deprecated deploy config and scripts

--- server/docker-deploy.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash -eu
-
-docker compose up --pull web-website web-assets web-crons web-renterd web-hostd web-walletd web-explorer web-explorer-testnet-zen
--- server/nx-build.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash -eu
-
-nx run-many --target=build --projects=website,assets,crons,renterd,hostd,walletd --prod
