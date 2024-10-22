5271e7392c333e2eb9709e8ef7ee25351664d94e
John Williams <john@sia.tech>
2024-10-22 11:01:08 -0400

    test(explorer): fix specificity issue with homepage test

--- apps/explorer-e2e/src/specs/index.spec.ts
+++ apps/explorer-e2e/src/specs/index.spec.ts
@@ -3,15 +3,15 @@ import { fillTextInputByName } from '@siafoundation/e2e'
 
 test('homepage', async ({ page }) => {
   await page.goto('/')
-  await expect(page.getByText('siascan')).toBeVisible()
-  await expect(page.getByText('zen')).toBeVisible()
+  await expect(page.getByTestId('explorer-identity-appName')).toBeVisible()
+  await expect(page.getByTestId('explorer-identity-network')).toBeVisible()
 })
 
 test('look up and view a block by block number', async ({ page }) => {
   await page.goto('/')
-  await fillTextInputByName(page, 'query', '3000')
+  await fillTextInputByName(page, 'query', '60000')
   await page.keyboard.press('Enter')
-  await expect(page.getByRole('link').getByText('3,000')).toBeVisible()
-  await expect(page.getByText('1 transactions')).toBeVisible()
-  await expect(page.getByText('000000...647f7f')).toBeVisible()
+  await expect(page.getByRole('link').getByText('60,000')).toBeVisible()
+  await expect(page.getByText('11 transactions')).toBeVisible()
+  await expect(page.getByText('000000...fa54c1')).toBeVisible()
 })
