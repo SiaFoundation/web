95dbaaeb9bb9279efb9bcdca1fad41844915dae5
John Williams <john@sia.tech>
2024-10-22 11:01:08 -0400

    test(explorer): fix specificity issue with homepage test

--- apps/explorer/components/Layout/Navbar.tsx
+++ apps/explorer/components/Layout/Navbar.tsx
@@ -16,25 +16,32 @@ type Props = {
 
 export function Navbar({ appName, network, homeHref, children }: Props) {
   return (
-    <div className={cx("py-3 z-10 bg-white dark:bg-graydark-50 border-b border-gray-200 dark:border-graydark-200",
-      network === 'mainnet' ? (
-        "border-t-4  border-t-green-500 dark:border-t-green-400"
-      ) : '',
-      network === 'zen' ? (
-        "border-t-4  border-t-amber-500 dark:border-t-amber-400"
-      ) : '',
-    )}>
+    <div
+      className={cx(
+        'py-3 z-10 bg-white dark:bg-graydark-50 border-b border-gray-200 dark:border-graydark-200',
+        network === 'mainnet'
+          ? 'border-t-4  border-t-green-500 dark:border-t-green-400'
+          : '',
+        network === 'zen'
+          ? 'border-t-4  border-t-amber-500 dark:border-t-amber-400'
+          : ''
+      )}
+    >
       <Container size="4" className="relative">
         <div className="flex items-center justify-between gap-2">
           <div className="relative z-10 flex">
             <Link href={homeHref} underline="none">
-              <div className="flex items-center gap-1.5 mr-2">
+              <div
+                className="flex items-center gap-1.5 mr-2"
+                data-testid="explorer-identity"
+              >
                 <Logo size={35} />
                 {appName && (
                   <Heading
                     font="mono"
                     size="24"
                     className="hidden pl-0 pr-px md:block tracking-tight relative -top-px"
+                    data-testid="explorer-identity-appName"
                   >
                     {appName}
                   </Heading>
@@ -43,6 +50,7 @@ export function Navbar({ appName, network, homeHref, children }: Props) {
                   <Text
                     className="bg-amber-500 dark:bg-amber-400 rounded py-0 px-2 hidden sm:flex"
                     color="lo"
+                    data-testid="explorer-identity-network"
                   >
                     {network}
                   </Text>
