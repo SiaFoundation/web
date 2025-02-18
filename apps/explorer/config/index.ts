547747a43feb7e546bb0b233934827bbc2c5a88b
John Williams <john@sia.tech>
2025-02-18 10:33:17 -0500

    feat(explorer): remove Sia Central dependency

--- apps/explorer/config/index.ts
+++ apps/explorer/config/index.ts
@@ -9,5 +9,4 @@ export const isMainnet = true
 
 // APIs
 export const faucetApi = 'https://api.siascan.com/zen/faucet'
-export const siaCentralApi = 'https://api.siacentral.com/v2'
 export const exploredApi = 'https://api.beta.siascan.com/api'
