a9863d07896b42d2cebab4f6264a83cf3b2526a9
Alex Freska <alex.freska@gmail.com>
2025-02-25 15:17:36 -0500

    chore: remove sia central libraries

--- libs/react-core/src/appSettings/useExternalData/types.ts
+++ libs/react-core/src/appSettings/useExternalData/types.ts
@@ -3,14 +3,12 @@ import { CurrencyId, CurrencyOption, currencyOptions } from './currency'
 export type CurrencyDisplay = 'sc' | 'fiat' | 'bothPreferSc' | 'bothPreferFiat'
 
 export type ExternalDataSettings = {
-  siaCentral: boolean
   siascan: boolean
   currency: CurrencyOption
   currencyDisplay: CurrencyDisplay
 }
 
 const defaultExternalDataSettings: ExternalDataSettings = {
-  siaCentral: true,
   siascan: true,
   currency: currencyOptions[0],
   currencyDisplay: 'bothPreferSc',
--- libs/sia-central-mock/README.md
+++
@@ -1,3 +0,0 @@
-# sia-central-mock
-
-Sia Central data and API mock library for testing.
