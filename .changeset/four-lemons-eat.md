45dde0cee3afc5addf2b0eb2a62bd15a2c5d4804
John Williams <john@sia.tech>
2025-03-18 17:13:06 -0400

    fix(explorer): make hardfork banner responsive

---
+++ .changeset/four-lemons-eat.md
@@ -0,0 +1,5 @@
+---
+'explorer': minor
+---
+
+Fixed hardfork countdown layout responsiveness. Closes https://github.com/SiaFoundation/web/issues/991.
--- apps/explorer/components/HardforkCountdown/index.tsx
+++ apps/explorer/components/HardforkCountdown/index.tsx
@@ -2,6 +2,7 @@ import { Text } from '@siafoundation/design-system'
 import { to } from '@siafoundation/request'
 import { getExplored } from '../../lib/explored'
 import clsx from 'clsx'
+import { blocksToDays } from '@siafoundation/units'
 
 function LearnMore({ color }: { color: string }) {
   return (
@@ -11,7 +12,7 @@ function LearnMore({ color }: { color: string }) {
       rel="noreferrer"
       className={clsx(
         'rounded py-1 px-2 inline-block font-sans text-white dark:text-graydark-50',
-        'font-normal text-sm inline-flex items-center gap-1 cursor-pointer',
+        'font-normal text-sm inline-flex items-center justify-center gap-1 cursor-pointer w-full max-w-[381px] sm:max-w-none sm:w-auto',
         color
       )}
     >
@@ -22,51 +23,51 @@ function LearnMore({ color }: { color: string }) {
 
 export async function HardforkCountdown({ network }: { network: string }) {
   const isMainnet = network === 'mainnet'
-  const [chainIndex, chainIndexError] = await to(getExplored().consensusTip())
+  const networkName = isMainnet ? 'mainnet' : 'the zen network'
+  const [
+    [chainIndex, chainIndexError],
+    [consensusNetwork, consensusNetworkError],
+  ] = await Promise.all([
+    to(getExplored().consensusTip()),
+    to(getExplored().consensusNetwork()),
+  ])
+
   if (chainIndexError || !chainIndex)
     throw new Error('explored consensusTip request failed in HardforkCountdown')
+  if (consensusNetworkError || !consensusNetwork)
+    throw new Error(
+      'explored consensusNetwork request failed in HardforkCountdown'
+    )
 
-  const allowHeight = isMainnet ? 526000 : 112000
-  const allowTime = new Date(
-    isMainnet ? '2025-06-06T02:00:00.000Z' : '2025-03-01T07:00:00.000Z'
-  )
-
-  const requireHeight = isMainnet ? 530000 : 114000
-  const requireTime = new Date(
-    isMainnet ? '2025-07-04T02:00:00.000Z' : '2025-03-15T06:00:00.000Z'
-  )
+  const allowHeight = consensusNetwork.hardforkV2.allowHeight
+  const requireHeight = consensusNetwork.hardforkV2.requireHeight
 
   const linkColor = isMainnet
     ? 'bg-green-500 dark:bg-green-400'
     : 'bg-amber-500 dark:bg-amber-400'
 
-  const currentTime = new Date()
-  const isRequireTimeActive = currentTime >= allowTime
-  const selectedTime = isRequireTimeActive ? requireTime : allowTime
-  const selectedHeight = isRequireTimeActive ? requireHeight : allowHeight
-
-  const daysRemaining = Math.ceil(
-    (selectedTime.getTime() - currentTime.getTime()) / (1000 * 60 * 60 * 24)
-  )
-
   if (chainIndex.height >= requireHeight) {
     return (
-      <div className="w-full py-3 flex gap-2 justify-center items-center bg-gray-100 dark:bg-graydark-50">
-        <Text color="contrast" weight="medium">
-          The V2 Hardfork update is now required on{' '}
-          {isMainnet ? 'mainnet' : 'the zen network'}
+      <div className="w-full px-5 py-3 flex flex-col sm:flex-row gap-2 justify-center items-center bg-gray-100 dark:bg-graydark-50">
+        <Text color="contrast" weight="medium" className="text-sm md:text-lg">
+          The V2 Hardfork update is now required on {networkName}
         </Text>
         <LearnMore color={linkColor} />
       </div>
     )
   }
 
+  const isRequireTimeActive = chainIndex.height >= allowHeight
+  const selectedHeight = isRequireTimeActive ? requireHeight : allowHeight
+  const blocksRemaining = selectedHeight - chainIndex.height
+  const daysRemaining = Math.ceil(blocksToDays(blocksRemaining))
+
   return (
-    <div className="w-full py-3 flex gap-2 justify-center items-center bg-gray-100 dark:bg-graydark-50">
-      <Text color="contrast" weight="medium">
+    <div className="w-full px-5 py-3 flex flex-col sm:flex-row gap-2 justify-center items-center bg-gray-100 dark:bg-graydark-50">
+      <Text color="contrast" weight="medium" className="text-sm md:text-lg">
         V2 Hardfork {isRequireTimeActive ? 'is required' : 'activates'} on{' '}
-        {isMainnet ? 'mainnet' : 'the zen network'} in {daysRemaining} days at
-        block height {selectedHeight.toLocaleString()}
+        {networkName} in {daysRemaining} days at block height{' '}
+        {selectedHeight.toLocaleString()}
       </Text>
       <LearnMore color={linkColor} />
     </div>
