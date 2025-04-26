8f22435a59ed97f1f158d5c40ecd636670a00bf0
Christopher Tarry <chrisjtarry@gmail.com>
2025-04-26 14:59:17 -0400

    feat(walletd): add support for v2 ledger transactions

---
+++ apps/walletd/lib/signLedgerV2.spec.ts
@@ -0,0 +1,78 @@
+import { initSDK } from '@siafoundation/sdk'
+import { signTransactionLedgerV2 } from './signLedgerV2'
+import { getMockAddresses, getMockDevice } from './testMocks'
+import { getMockScenarioSeedWallet } from './mocks/mockSeedWallet'
+
+beforeEach(async () => {
+  await initSDK()
+})
+
+describe('signLedgerV2', () => {
+  describe('siacoin', () => {
+    it('builds and signs valid transaction', async () => {
+      const device = getMockDevice()
+      const mocks = getMockScenarioSeedWallet()
+      expect(
+        await signTransactionLedgerV2({
+          device,
+          transaction: mocks.walletConstructV2Response.transaction,
+          addresses: getMockAddresses(mocks),
+          siacoinOutputs: mocks.walletOutputsSiacoinResponse.outputs,
+          siafundOutputs: mocks.walletOutputsSiafundResponse.outputs,
+        })
+      ).toMatchSnapshot()
+    })
+
+    it('errors when a public keys address is missing', async () => {
+      const device = getMockDevice()
+      const mocks = getMockScenarioSeedWallet()
+      expect(
+        await signTransactionLedgerV2({
+          device,
+          transaction: mocks.walletConstructV2Response.transaction,
+          addresses: [
+            {
+              id: 'id',
+              walletId: 'id',
+              address: 'address not in addresses',
+              metadata: {
+                index: 5,
+              },
+            },
+          ],
+          siacoinOutputs: mocks.walletOutputsSiacoinResponse.outputs,
+          siafundOutputs: mocks.walletOutputsSiafundResponse.outputs,
+        })
+      ).toEqual({
+        error:
+          'Missing address 90c6057cdd2463eca61f83796e83152dbba28b6cb9a74831a043833051ec9f422726bfff2ee8',
+      })
+    })
+
+    it('errors when an address is missing its index', async () => {
+      const device = getMockDevice()
+      const mocks = getMockScenarioSeedWallet()
+      expect(
+        await signTransactionLedgerV2({
+          device,
+          transaction: mocks.walletConstructV2Response.transaction,
+          addresses: [
+            {
+              id: 'id',
+              walletId: 'id',
+              address:
+                mocks.walletOutputsSiacoinResponse.outputs[1].siacoinOutput
+                  .address,
+              metadata: {},
+            },
+          ],
+          siacoinOutputs: mocks.walletOutputsSiacoinResponse.outputs,
+          siafundOutputs: mocks.walletOutputsSiafundResponse.outputs,
+        })
+      ).toEqual({
+        error:
+          'Missing address 90c6057cdd2463eca61f83796e83152dbba28b6cb9a74831a043833051ec9f422726bfff2ee8',
+      })
+    })
+  })
+})
