ea87a90b6ef29942cf0f272c079324d4d6d625fd
John Williams <john@sia.tech>
2025-05-20 16:34:01 -0400

    test(explorer): add v2 coverage

--- apps/explorer-e2e/src/specs/home.spec.ts
+++ apps/explorer-e2e/src/specs/home.spec.ts
@@ -16,45 +16,92 @@ import { exploredStabilization } from '../helpers/exploredStabilization'
 let explorerApp: ExplorerApp
 let cluster: Cluster
 
-test.beforeEach(async ({ page, context }) => {
-  cluster = await startCluster({ context })
-  await exploredStabilization(cluster)
+// V2
+test.describe('v2', () => {
+  test.beforeEach(async ({ page, context }) => {
+    cluster = await startCluster({ context, networkVersion: 'v2' })
+    await exploredStabilization(cluster)
+    explorerApp = new ExplorerApp(page)
+    await explorerApp.goTo('/')
+  })
 
-  explorerApp = new ExplorerApp(page)
+  test.afterEach(async () => {
+    await teardownCluster()
+  })
 
-  // All these tests will start and end on index.
-  await explorerApp.goTo('/')
+  test('home displays app identity', async ({ page }) => {
+    await expect(page.getByTestId(APP_NAME)).toContainText('siascan')
+    await expect(page.getByTestId(APP_NETWORK)).toContainText('zen')
+  })
+
+  test('home displays metrics', async ({ page }) => {
+    expect((await page.getByTestId(METRICS_ITEM).count()) > 0).toBeTruthy()
+  })
+
+  test('home displays latest blocks', async ({ page }) => {
+    expect(
+      (await page.getByTestId(LATEST_BLOCKS_ITEM).count()) > 0
+    ).toBeTruthy()
+  })
+
+  test('home displays top hosts', async ({ page }) => {
+    expect((await page.getByTestId(TOP_HOSTS_ITEMS).count()) > 0).toBeTruthy()
+  })
+
+  test('home displays accurate network information', async ({ page }) => {
+    const activeHostCount = cluster.daemons.hostds.length
+    const {
+      data: { height },
+    } = await cluster.daemons.hostds[0].api.consensusTip()
+
+    await expect(page.getByTestId(HOME_ACTIVE_HOSTS)).toContainText(
+      String(activeHostCount)
+    )
+    await expect(page.getByTestId(HOME_HEIGHT)).toContainText(String(height))
+  })
 })
 
-test.afterEach(async () => {
-  await teardownCluster()
-})
+// V1
+test.describe('v1', () => {
+  test.beforeEach(async ({ page, context }) => {
+    cluster = await startCluster({ context, networkVersion: 'v1' })
+    await exploredStabilization(cluster)
+    explorerApp = new ExplorerApp(page)
+    await explorerApp.goTo('/')
+  })
 
-test('home displays app identity', async ({ page }) => {
-  await expect(page.getByTestId(APP_NAME)).toContainText('siascan')
-  await expect(page.getByTestId(APP_NETWORK)).toContainText('zen')
-})
+  test.afterEach(async () => {
+    await teardownCluster()
+  })
 
-test('home displays metrics', async ({ page }) => {
-  expect((await page.getByTestId(METRICS_ITEM).count()) > 0).toBeTruthy()
-})
+  test('home displays app identity', async ({ page }) => {
+    await expect(page.getByTestId(APP_NAME)).toContainText('siascan')
+    await expect(page.getByTestId(APP_NETWORK)).toContainText('zen')
+  })
 
-test('home displays latest blocks', async ({ page }) => {
-  expect((await page.getByTestId(LATEST_BLOCKS_ITEM).count()) > 0).toBeTruthy()
-})
+  test('home displays metrics', async ({ page }) => {
+    expect((await page.getByTestId(METRICS_ITEM).count()) > 0).toBeTruthy()
+  })
 
-test('home displays top hosts', async ({ page }) => {
-  expect((await page.getByTestId(TOP_HOSTS_ITEMS).count()) > 0).toBeTruthy()
-})
+  test('home displays latest blocks', async ({ page }) => {
+    expect(
+      (await page.getByTestId(LATEST_BLOCKS_ITEM).count()) > 0
+    ).toBeTruthy()
+  })
 
-test('home displays accurate network information', async ({ page }) => {
-  const activeHostCount = cluster.daemons.hostds.length
-  const {
-    data: { height },
-  } = await cluster.daemons.hostds[0].api.consensusTip()
+  test('home displays top hosts', async ({ page }) => {
+    expect((await page.getByTestId(TOP_HOSTS_ITEMS).count()) > 0).toBeTruthy()
+  })
 
-  await expect(page.getByTestId(HOME_ACTIVE_HOSTS)).toContainText(
-    String(activeHostCount)
-  )
-  await expect(page.getByTestId(HOME_HEIGHT)).toContainText(String(height))
+  test('home displays accurate network information', async ({ page }) => {
+    const activeHostCount = cluster.daemons.hostds.length
+    const {
+      data: { height },
+    } = await cluster.daemons.hostds[0].api.consensusTip()
+
+    await expect(page.getByTestId(HOME_ACTIVE_HOSTS)).toContainText(
+      String(activeHostCount)
+    )
+    await expect(page.getByTestId(HOME_HEIGHT)).toContainText(String(height))
+  })
 })
