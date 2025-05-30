7d0bf943901f918a8d3294b452a98b036430d0a8
John Williams <john@sia.tech>
2025-05-30 19:07:28 -0400

    test(explorer): expand contract status testing

--- apps/explorer-e2e/src/specs/contract.spec.ts
+++ apps/explorer-e2e/src/specs/contract.spec.ts
@@ -6,6 +6,11 @@ import {
   teardownCluster,
 } from '@siafoundation/clusterd'
 import { exploredStabilization } from '../helpers/exploredStabilization'
+import {
+  findV1TestContractWithResolutionType,
+  findV2TestContractWithResolutionType,
+} from '../helpers/findTestContract'
+import { RENEWED_FROM_BUTTON, RENEWED_TO_BUTTON } from '../fixtures/constants'
 
 let explorerApp: ExplorerApp
 let cluster: Cluster
@@ -56,6 +61,54 @@ test.describe('v2', () => {
         .getByText('Contract ' + contractID.slice(0, 5))
     ).toBeVisible()
   })
+
+  test('contract correctly displays a completed contract', async ({ page }) => {
+    const completedContract = await findV2TestContractWithResolutionType(
+      cluster,
+      'storage_proof'
+    )
+    await explorerApp.goTo('/contract/' + completedContract?.id)
+
+    await expect(page.getByText('complete')).toBeVisible()
+  })
+
+  test('contract correctly displays a failed contract', async ({ page }) => {
+    const failedContract = await findV2TestContractWithResolutionType(
+      cluster,
+      'expiration'
+    )
+    await explorerApp.goTo('/contract/' + failedContract?.id)
+
+    await expect(page.getByText('failed')).toBeVisible()
+  })
+
+  test('contract correctly displays a contract in progress', async ({
+    page,
+  }) => {
+    const renewedContract = await findV2TestContractWithResolutionType(
+      cluster,
+      'renewal'
+    )
+    await explorerApp.goTo('/contract/' + renewedContract?.renewedTo)
+    await expect(page.getByText('in progress')).toBeVisible()
+  })
+
+  test('contract navigate to and from a renewed contract', async ({ page }) => {
+    const renewedContract = await findV2TestContractWithResolutionType(
+      cluster,
+      'renewal'
+    )
+    console.log(renewedContract?.id)
+    await explorerApp.goTo('/contract/' + renewedContract?.id)
+    await page.getByTestId(RENEWED_TO_BUTTON).click()
+    await expect(
+      page.getByText(renewedContract?.renewedTo?.slice(0, 6) || 'TEST FAIL')
+    ).toBeVisible()
+    await page.getByTestId(RENEWED_FROM_BUTTON).click()
+    await expect(
+      page.getByText(renewedContract?.id.slice(0, 6) || 'TEST FAIL').first()
+    ).toBeVisible()
+  })
 })
 
 // V1
@@ -103,5 +156,39 @@ test.describe('v1', () => {
         .getByTestId('entity-heading')
         .getByText('Contract ' + contractID.slice(0, 5))
     ).toBeVisible()
+  })
+
+  test('contract correctly displays a completed contract', async ({ page }) => {
+    const completedContract = await findV1TestContractWithResolutionType(
+      cluster,
+      'complete'
+    )
+    await explorerApp.goTo('/contract/' + completedContract?.id)
+
+    await expect(page.getByText('complete')).toBeVisible()
+  })
+
+  test('contract correctly displays a failed contract', async ({ page }) => {
+    const failedContract = await findV1TestContractWithResolutionType(
+      cluster,
+      'failed'
+    )
+
+    await explorerApp.goTo('/contract/' + failedContract?.id)
+
+    await expect(page.getByText('failed')).toBeVisible()
+  })
+
+  test('contract correctly displays a contract in progress', async ({
+    page,
+  }) => {
+    const inProgressContract = await findV1TestContractWithResolutionType(
+      cluster,
+      'in progress'
+    )
+
+    await explorerApp.goTo('/contract/' + inProgressContract?.id)
+
+    await expect(page.getByText('in progress')).toBeVisible()
   })
 })
