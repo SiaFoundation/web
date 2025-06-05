941f2fd48b75b5c132a1f7404fc94095962ef1b0
John Williams <john@sia.tech>
2025-06-05 12:23:10 -0400

    feat(explorer): add dropdown for JSON, add to block and tx pages

---
+++ apps/explorer/components/ExplorerAccordion.tsx
@@ -0,0 +1,29 @@
+import {
+  Accordion,
+  AccordionItem,
+  AccordionTrigger,
+  AccordionContent,
+  Heading,
+} from '@siafoundation/design-system'
+
+type ExplorerAccordionProps = {
+  title: string
+  children: React.ReactNode
+}
+
+export function ExplorerAccordion({ title, children }: ExplorerAccordionProps) {
+  return (
+    <Accordion type="single">
+      <AccordionItem value="steps" variant="ghost">
+        <div className="transition-shadow ease-in-out duration-300 shadow-sm hover:shadow rounded border border-gray-400 dark:border-graydark-400 bg-white dark:bg-graydark-200 overflow-hidden">
+          <AccordionTrigger className="flex items-center p-4 border-b border-gray-200 dark:border-graydark-300 w-full focus:outline-none focus:ring-2 focus:ring-gray-400 dark:focus:ring-graydark-400">
+            <Heading size="20" font="mono" ellipsis>
+              {title}
+            </Heading>
+          </AccordionTrigger>
+          <AccordionContent>{children}</AccordionContent>
+        </div>
+      </AccordionItem>
+    </Accordion>
+  )
+}
