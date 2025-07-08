45b0087181de725b97e3804b3321cb4927918213
Alex Freska <alex.freska@gmail.com>
2025-07-08 10:49:20 -0400

    fix(data-sources): remove deprecated code

--- libs/data-sources/src/lib/assets.ts
+++
@@ -1,9 +0,0 @@
-import * as path from 'path'
-
-export function getAssetPath(filePath: string) {
-  return path.join(getAssetsDirectory(), filePath)
-}
-
-export function getAssetsDirectory() {
-  return process.env['ASSETS'] || '/assets'
-}
