fec3db23e540acde9695b8b2ff5c50432ab6226a
Alex Freska <alex.freska@gmail.com>
2025-07-18 09:29:58 -0400

    fix(renterd): remove unused file

--- apps/renterd/components/Hosts/HostMap/GlobeDyn.tsx
+++
@@ -1,9 +0,0 @@
-import dynamic from 'next/dynamic'
-
-const GlobeGl = dynamic(() => import('./GlobeImp'), {
-  ssr: false,
-})
-
-export function GlobeDyn(props: React.ComponentProps<typeof GlobeGl>) {
-  return <GlobeGl {...props} />
-}
