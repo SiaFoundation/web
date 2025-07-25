dca3fab9501ab57bc17d1a081dfaa374beeaf98a
Alex Freska <alex.freska@gmail.com>
2025-07-25 13:51:48 -0400

    feat(indexd): cmdk menu

---
+++ apps/indexd/components/CmdRoot/ConfigCmdGroup.tsx
@@ -0,0 +1,75 @@
+import { routes } from '../../config/routes'
+import { useRouter } from 'next/navigation'
+import { useDialog } from '../../contexts/dialog'
+import { CommandGroup, CommandItemNav, CommandItemSearch } from './Item'
+import { Page } from './types'
+
+const commandPage = {
+  namespace: 'configuration',
+  label: 'Configuration',
+}
+
+type Props = {
+  currentPage: Page
+  parentPage?: Page
+  pushPage: (page: Page) => void
+}
+
+export function ConfigCmdGroup({ currentPage, parentPage, pushPage }: Props) {
+  const router = useRouter()
+  const { closeDialog } = useDialog()
+  return (
+    <CommandGroup currentPage={currentPage} commandPage={commandPage}>
+      <CommandItemNav
+        currentPage={currentPage}
+        parentPage={parentPage}
+        commandPage={commandPage}
+        onSelect={() => {
+          pushPage(commandPage)
+        }}
+      >
+        {commandPage.label}
+      </CommandItemNav>
+      <CommandItemSearch
+        currentPage={currentPage}
+        commandPage={commandPage}
+        onSelect={() => {
+          router.push(routes.config.index)
+          closeDialog()
+        }}
+      >
+        Open configuration
+      </CommandItemSearch>
+      <CommandItemSearch
+        currentPage={currentPage}
+        commandPage={commandPage}
+        onSelect={() => {
+          router.push(routes.config.contracts)
+          closeDialog()
+        }}
+      >
+        Configure contracts
+      </CommandItemSearch>
+      <CommandItemSearch
+        currentPage={currentPage}
+        commandPage={commandPage}
+        onSelect={() => {
+          router.push(routes.config.hosts)
+          closeDialog()
+        }}
+      >
+        Configure hosts
+      </CommandItemSearch>
+      <CommandItemSearch
+        currentPage={currentPage}
+        commandPage={commandPage}
+        onSelect={() => {
+          router.push(routes.config.pricePinning)
+          closeDialog()
+        }}
+      >
+        Configure price pinning
+      </CommandItemSearch>
+    </CommandGroup>
+  )
+}
---
+++ apps/indexd/components/CmdRoot/NodeCmdGroup.tsx
@@ -0,0 +1,64 @@
+import { routes } from '../../config/routes'
+import { useRouter } from 'next/navigation'
+import { useDialog } from '../../contexts/dialog'
+import { CommandGroup, CommandItemNav, CommandItemSearch } from './Item'
+import { Page } from './types'
+
+const commandPage = {
+  namespace: 'node',
+  label: 'Blockchain node',
+}
+
+type Props = {
+  currentPage: Page
+  parentPage?: Page
+  pushPage: (page: Page) => void
+}
+
+export function NodeCmdGroup({ currentPage, parentPage, pushPage }: Props) {
+  const { openDialog, closeDialog } = useDialog()
+  const router = useRouter()
+  return (
+    <CommandGroup currentPage={currentPage} commandPage={commandPage}>
+      <CommandItemNav
+        currentPage={currentPage}
+        parentPage={parentPage}
+        commandPage={commandPage}
+        onSelect={() => {
+          pushPage(commandPage)
+        }}
+      >
+        {commandPage.label}
+      </CommandItemNav>
+      <CommandItemSearch
+        currentPage={currentPage}
+        commandPage={commandPage}
+        onSelect={() => {
+          openDialog('connectPeer')
+        }}
+      >
+        Connect to a peer
+      </CommandItemSearch>
+      <CommandItemSearch
+        currentPage={currentPage}
+        commandPage={commandPage}
+        onSelect={() => {
+          router.push(routes.node.index)
+          closeDialog()
+        }}
+      >
+        View peers
+      </CommandItemSearch>
+      <CommandItemSearch
+        currentPage={currentPage}
+        commandPage={commandPage}
+        onSelect={() => {
+          router.push(routes.node.index)
+          closeDialog()
+        }}
+      >
+        View transaction pool
+      </CommandItemSearch>
+    </CommandGroup>
+  )
+}
