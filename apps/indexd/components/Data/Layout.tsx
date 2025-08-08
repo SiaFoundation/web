e91b293f694fe4c210f5d2a690645c445a9afc41
Alex Freska <alex.freska@gmail.com>
2025-08-08 13:08:24 -0400

    feat(indexd): resizable data panel

---
+++ apps/indexd/components/Data/Layout.tsx
@@ -0,0 +1,26 @@
+import { Panel, PanelGroup, PanelResizeHandle } from 'react-resizable-panels'
+
+export function Layout({
+  table,
+  panel,
+}: {
+  table: React.ReactNode
+  panel: React.ReactNode
+}) {
+  return (
+    <PanelGroup autoSaveId="data-layout" direction="horizontal">
+      <Panel maxSize={75} className="py-4 pl-4 pr-2">
+        {table}
+      </Panel>
+      <PanelResizeHandle />
+      <Panel
+        defaultSize={25}
+        minSize={25}
+        maxSize={50}
+        className="py-4 pl-2 pr-4"
+      >
+        {panel}
+      </Panel>
+    </PanelGroup>
+  )
+}
