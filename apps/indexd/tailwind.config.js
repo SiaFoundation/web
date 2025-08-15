c2521c39061096c95fd29fc8d58f1358452f04f1
Alex Freska <alex.freska@gmail.com>
2025-08-15 18:07:11 -0400

    fix(tailwind): jit detects changes

--- apps/indexd/tailwind.config.js
+++ apps/indexd/tailwind.config.js
@@ -1,9 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    __dirname +
-      '/{src,app,pages,components,hooks,contexts,dialogs,config,lib}/**/*!(*.stories|*.spec).{ts,tsx,html}',
-    '../../libs/design-system/src/**/*!(*.stories|*.spec).{ts,tsx,html}',
+    './{src,app,pages,components,hooks,contexts,dialogs,config,lib}/**/*.{ts,tsx,html}',
+    '!./{src,app,pages,components,hooks,contexts,dialogs,config,lib}/**/*.stories.{ts,tsx,html}',
+    '!./{src,app,pages,components,hooks,contexts,dialogs,config,lib}/**/*.spec.{ts,tsx,html}',
+    '../../libs/design-system/src/**/*.{ts,tsx,html}',
+    '!../../libs/design-system/src/**/*.stories.{ts,tsx,html}',
+    '!../../libs/design-system/src/**/*.spec.{ts,tsx,html}',
   ],
   presets: [require('../../libs/design-system/src/style/theme.js')],
 }
--- libs/design-system/tailwind.config.js
+++ libs/design-system/tailwind.config.js
@@ -1,6 +1,10 @@
 // This file provides tailwind intellisense within design-system src files.
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./src/**/*!(*.stories|*.spec).{ts,tsx,html}'],
+  content: [
+    './src/**/*.{ts,tsx,html}',
+    '!./src/**/*.stories.{ts,tsx,html}',
+    '!./src/**/*.spec.{ts,tsx,html}',
+  ],
   presets: [require('./src/style/theme.js')],
 }
