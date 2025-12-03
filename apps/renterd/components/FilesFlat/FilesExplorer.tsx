bda95720a1d78cc3fe0a85fc438545b7df2e3248
Alex Freska <alex.freska@gmail.com>
2025-12-03 11:01:50 -0800

    feat(renterd): upload files in all files mode

--- apps/renterd/components/FilesFlat/FilesExplorer.tsx
+++ apps/renterd/components/FilesFlat/FilesExplorer.tsx
@@ -1,25 +1,42 @@
-import { Table } from '@siafoundation/design-system'
+import { Table, Dropzone } from '@siafoundation/design-system'
 import { EmptyState } from './EmptyState'
 import { useFilesFlat } from '../../contexts/filesFlat'
+import { useCanUpload } from '../Files/useCanUpload'
+import { useUploadsManager } from '../../contexts/uploadsManager'
 
 export function FilesExplorer() {
-  const { datasetPage, datasetState, cellContext, tableState } = useFilesFlat()
+  const {
+    datasetPage,
+    datasetState,
+    cellContext,
+    tableState,
+    datasetPageTotal,
+  } = useFilesFlat()
+  const { uploadFiles } = useUploadsManager()
+  const canUpload = useCanUpload()
   return (
     <div className="relative">
-      <Table
-        testId="filesTable"
-        isLoading={datasetState === 'loading'}
-        emptyState={<EmptyState />}
-        pageSize={10}
-        data={datasetPage}
-        context={cellContext}
-        columns={tableState.visibleColumns}
-        sortableColumns={tableState.sortableColumns}
-        sortField={tableState.sortField}
-        sortDirection={tableState.sortDirection}
-        toggleSort={tableState.toggleSort}
-        rowSize="dense"
-      />
+      <Dropzone
+        testId="filesDropzone"
+        onDrop={uploadFiles}
+        noClick={!canUpload || datasetPageTotal > 0}
+        noDrag={!canUpload}
+      >
+        <Table
+          testId="filesTable"
+          isLoading={datasetState === 'loading'}
+          emptyState={<EmptyState />}
+          pageSize={10}
+          data={datasetPage}
+          context={cellContext}
+          columns={tableState.visibleColumns}
+          sortableColumns={tableState.sortableColumns}
+          sortField={tableState.sortField}
+          sortDirection={tableState.sortDirection}
+          toggleSort={tableState.toggleSort}
+          rowSize="dense"
+        />
+      </Dropzone>
     </div>
   )
 }
--- apps/renterd/components/FilesFlat/FilesFlatActionsMenu.tsx
+++ apps/renterd/components/FilesFlat/FilesFlatActionsMenu.tsx
@@ -1,12 +1,26 @@
 import { Button } from '@siafoundation/design-system'
-import { Search16 } from '@siafoundation/react-icons'
+import { CloudUpload16, Search16 } from '@siafoundation/react-icons'
+import * as reactDropzone from 'react-dropzone'
 import { useDialog } from '../../contexts/dialog'
 import { FilesViewDropdownMenu } from '../Files/FilesViewDropdownMenu'
 import { useFilesFlat } from '../../contexts/filesFlat'
+import { useCanUpload } from '../Files/useCanUpload'
+import { useUploadsManager } from '../../contexts/uploadsManager'
+// esm compat
+const { useDropzone } = reactDropzone
 
 export function FilesFlatActionsMenu() {
   const { openDialog } = useDialog()
   const { tableState } = useFilesFlat()
+  const { uploadFiles } = useUploadsManager()
+  const canUpload = useCanUpload()
+
+  const { getRootProps, getInputProps } = useDropzone({
+    noDrag: true,
+    noClick: !canUpload,
+    onDrop: uploadFiles,
+  })
+
   return (
     <div className="flex gap-2">
       <Button
@@ -15,6 +29,15 @@ export function FilesFlatActionsMenu() {
         aria-label="search files"
       >
         <Search16 />
+      </Button>
+      <Button
+        aria-label="Upload files"
+        {...getRootProps()}
+        tip="Upload files"
+        disabled={!canUpload}
+      >
+        <input {...getInputProps()} />
+        <CloudUpload16 />
       </Button>
       <FilesViewDropdownMenu tableState={tableState} />
     </div>
