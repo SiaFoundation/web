d01fa1e40d80974bb9d75da7d57e6675a0235b99
Alex Freska <alex.freska@gmail.com>
2025-12-04 07:52:13 -0800

    fix(explorer): add error page to route groups

---
+++ apps/explorer/app/(main)/error.tsx
@@ -0,0 +1,13 @@
+'use client'
+
+import { StateError } from '../../components/StateError'
+
+export default function Page({ error }: { error: Error }) {
+  console.error(error)
+  return (
+    <StateError
+      code={500}
+      message="Error, the explorer is having issues loading data."
+    />
+  )
+}
