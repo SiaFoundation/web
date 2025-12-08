2514de0cf04344c16a619ddd7bf84dac441945d7
John Williams <john@sia.tech>
2025-12-08 10:37:16 -0500

    refactor(explorer): move getHostNetAddress away from throwing

---
+++ .changeset/seven-colts-care.md
@@ -0,0 +1,5 @@
+---
+'explorer': minor
+---
+
+Host address rendering now prioritizes siamux, quic, and finally, displays 'unknown' if neither are found.
--- apps/explorer/lib/hostType.ts
+++ apps/explorer/lib/hostType.ts
@@ -1,16 +1,50 @@
 import { ExplorerHost } from '@siafoundation/explored-types'
 
-export function getHostNetAddress(host: ExplorerHost) {
-  let netAddress: string | undefined
-
+export function getHostNetAddress(host: ExplorerHost): string {
+  // v2
   if (host.v2) {
-    netAddress = host.v2NetAddresses.find(
-      (address) => address.protocol === 'siamux',
+    // Prefer siamux.
+    const siamux = host.v2NetAddresses.find(
+      (addr) => addr.protocol === 'siamux',
     )?.address
-  } else {
-    netAddress = host.settings.netaddress
+
+    if (siamux) {
+      return siamux
+    }
+
+    // Fallback to quic.
+    const quic = host.v2NetAddresses.find(
+      (addr) => addr.protocol === 'quic',
+    )?.address
+
+    if (quic) {
+      console.warn('Falling back to quic address for v2 host (no siamux)', {
+        publicKey: host.publicKey,
+        v2NetAddresses: host.v2NetAddresses,
+      })
+      return quic
+    }
+
+    // Log if we can't find either one, which should be unlikely.
+    console.warn('Missing siamux/quic address for v2 host', {
+      publicKey: host.publicKey,
+      v2NetAddresses: host.v2NetAddresses,
+    })
+
+    return 'unknown'
   }
 
-  if (!netAddress) throw new Error('netAddress undefined in getHostNetAddress')
-  return netAddress
+  // v1
+  if (host.netAddress) {
+    return host.netAddress
+  }
+
+  // Log if we can't find netAddress on a v1 host, but this should be highly unlikely.
+  console.warn('Missing netAddress for v1 host', {
+    publicKey: host.publicKey,
+    netAddress: host.netAddress,
+    settings: host.settings,
+  })
+
+  return 'unknown'
 }
